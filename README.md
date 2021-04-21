e0c45672ab71a05bd269fe568317cc3b545d9c6a
Laxmichandra Dhuvare <laxmichandra.dhuvare@godigit.com>
2021-04-22 01:05:18 +0530

    with watch dist folder

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bookStore
+
 This is online bookstore Demo Application.
+
+to Start Frontend :
+inside frontend folder
+
+ng build --output-path dist --watch
+
+To Start backend :
+
+nodemon server.js
--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-console.log("server workes");
+const express = require("express");
+const app = express();
+var path = require("path");
+const bodyParser = require("body-parser");
+const { port, endpoint } = require("./backend/src/config/config");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "5mb" }));
+
+app.options("/*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.sendStatus(200);
+  next();
+});
+
+app.use("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get(["/"], (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key,token,Origin,X-Origin"
+  );
+  res.sendFile("index.html", { root: __dirname + "/frontend/dist" });
+});
+
+app.listen(`${port}`, function () {
+  console.log(
+    "server deployed on http://%s:%s",
+    this.address().address,
+    this.address().port
+  );
+});
