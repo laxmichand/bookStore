debdfe8dd4841ea9e2d2a7b99b4ebed82235688a
Laxmichandra Dhuvare <laxmichandra.dhuvare@godigit.com>
2021-04-23 02:21:42 +0530

    db iisue

--- backend/src/config/projectconfig.js
+++ backend/src/config/projectconfig.js
@@ -4,10 +4,11 @@ module.exports = {
   },
   db_connection: {
     production: {
-      server: "@ds151626.mlab.com:51626",
-      database: "book",
-      username: "book",
-      password: "book",
+      uri='mongodb+srv://book:book@cluster0.ijz9u.mongodb.net/books?retryWrites=true&w=majority'
+      // server: "@ds151626.mlab.com:51626",
+      // database: "book",
+      // username: "book",
+      // password: "book",
     },
     development: {
       server: "127.0.0.1:27017",
--- backend/src/utility/dbConnection.js
+++ backend/src/utility/dbConnection.js
@@ -2,9 +2,8 @@ let mongoose = require("mongoose");
 const { endpoint } = require("../config/config");
 const dbConn = require("../config/projectconfig").db_connection;
 
-if (endpoint === "development1") {
+if (endpoint === "development") {
   var url = `mongodb://${dbConn.development.server}/${dbConn.development.database}`;
-  var url = `mongodb+srv://book:book@cluster0.ijz9u.mongodb.net/books?retryWrites=true&w=majority`;
   // var url=`mongodb://${dbConn.production.username}:${dbConn.production.password}${dbConn.production.server}/${dbConn.production.database}`
 } else {
   // var url = `mongodb://${dbConn.production.username}:${dbConn.production.password}${dbConn.production.server}/${dbConn.production.database}`;
