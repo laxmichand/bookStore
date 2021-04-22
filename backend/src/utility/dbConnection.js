92e1244a5544d94359b7cd7f930b5f4f1ad4dad7
Laxmichandra Dhuvare <laxmichandra.dhuvare@godigit.com>
2021-04-23 02:10:28 +0530

    db issue

--- backend/src/utility/dbConnection.js
+++ backend/src/utility/dbConnection.js
@@ -2,8 +2,9 @@ let mongoose = require("mongoose");
 const { endpoint } = require("../config/config");
 const dbConn = require("../config/projectconfig").db_connection;
 
-if (endpoint === "development") {
+if (endpoint === "development1") {
   var url = `mongodb://${dbConn.development.server}/${dbConn.development.database}`;
+  var url = `mongodb+srv://book:book@cluster0.ijz9u.mongodb.net/books?retryWrites=true&w=majority`;
   // var url=`mongodb://${dbConn.production.username}:${dbConn.production.password}${dbConn.production.server}/${dbConn.production.database}`
 } else {
   // var url = `mongodb://${dbConn.production.username}:${dbConn.production.password}${dbConn.production.server}/${dbConn.production.database}`;
