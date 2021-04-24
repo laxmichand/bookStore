5b2222be840705b4997328c71a884404b81c1169
Laxmichandra Dhuvare <laxmichandra.dhuvare@godigit.com>
2021-04-24 19:24:16 +0530

    about icon

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  gotoPortfolio() {
+    window.open('http://laxmichand.surge.sh/', '_blank');
   }
-
 }
