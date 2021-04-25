4f0bbf81bd4f97240c427aa48201799adfc32873
Laxmichandra Dhuvare <laxmichandra.dhuvare@godigit.com>
2021-04-25 12:42:40 +0530

    master

--- backend/src/config/projectconfig.js
+++ backend/src/config/projectconfig.js
@@ -6,10 +6,6 @@ module.exports = {
     production: {
       uri:
         "mongodb+srv://book:book@cluster0.ijz9u.mongodb.net/books?retryWrites=true&w=majority",
-      // server: "@cluster0.ijz9u.mongodb.net/",
-      // database: "book",
-      // username: "book",
-      // password: "book",
     },
     development: {
       server: "127.0.0.1:27017",
--- backend/src/utility/dbConnection.js
+++ backend/src/utility/dbConnection.js
@@ -4,9 +4,7 @@ const dbConn = require("../config/projectconfig").db_connection;
 
 if (endpoint === "development") {
   var url = `mongodb://${dbConn.development.server}/${dbConn.development.database}`;
-  // var url=`mongodb://${dbConn.production.username}:${dbConn.production.password}${dbConn.production.server}/${dbConn.production.database}`
 } else {
-  // var url = `mongodb://${dbConn.production.username}:${dbConn.production.password}${dbConn.production.server}/${dbConn.production.database}`;
   var url = `mongodb+srv://book:book@cluster0.ijz9u.mongodb.net/books?retryWrites=true&w=majority`;
 }
 console.log(url);
